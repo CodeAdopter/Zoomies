e98798a0bba506c8fa850155b0d8a1b730c69644
logapsystems <logapsystems@gmail.com>
2026-07-31 10:16:55 +0200

    Version 1.10 [+250] PeSTO
    Results of Zoomies v1.10 vs Zoomies v1.9 (5+0.1, NULL, NULL, UHO_Lichess_4852_v1.epd):
    Elo: 250.31 +/- 40.12, nElo: 302.90 +/- 34.75
    LOS: 100.00 %, DrawRatio: 17.71 %, PairsRatio: 12.17
    Games: 384, Wins: 284, Losses: 47, Draws: 53, Points: 310.5 (80.86 %)
    Ptnml(0-2): [3, 9, 34, 40, 106], WL/DD Ratio: 16.00
    LLR: 2.95 (100.3%) (-2.94, 2.94) [0.00, 5.00]

--- src/Engine/Eval.cs
+++ src/Engine/Eval.cs
@@ -4,25 +4,41 @@ namespace Zoomies.Engine;
 
 public static class Eval
 {
-    public static readonly int[] PieceValue = Psqt.PieceValue;
+    public static readonly int[] PieceValue = [100, 320, 330, 500, 900, 0];
 
     public const int MateValue = 30000;
     public const int MateBound = MateValue - 1000;
 
-    public static int Evaluate(Position pos) =>
-        pos.Turn == Color.White ? pos.WhiteEvaluation : -pos.WhiteEvaluation;
+    public static int Evaluate(Position pos) => Taper(pos, pos.WhiteEvaluation);
 
     public static int EvaluateScratch(Position pos)
     {
-        int whitePov = 0;
+        int packed = 0;
         for (int c = 0; c < 2; c++)
             for (int pt = 0; pt < 6; pt++)
             {
                 ulong bb = pos.BitboardOf((Color)c, (PieceType)pt);
                 Piece pc = Types.MakePiece((Color)c, (PieceType)pt);
                 while (bb != 0)
-                    whitePov += Psqt.Value(pc, Bitboard.PopLsb(ref bb));
+                    packed += Psqt.Value(pc, Bitboard.PopLsb(ref bb));
             }
+        return Taper(pos, packed);
+    }
+
+    private static int Taper(Position pos, int packed)
+    {
+        int minors = Bitboard.PopCount(
+            pos.BitboardOf(Color.White, PieceType.Knight) | pos.BitboardOf(Color.Black, PieceType.Knight) |
+            pos.BitboardOf(Color.White, PieceType.Bishop) | pos.BitboardOf(Color.Black, PieceType.Bishop));
+        int rooks = Bitboard.PopCount(
+            pos.BitboardOf(Color.White, PieceType.Rook)   | pos.BitboardOf(Color.Black, PieceType.Rook));
+        int queens = Bitboard.PopCount(
+            pos.BitboardOf(Color.White, PieceType.Queen)  | pos.BitboardOf(Color.Black, PieceType.Queen));
+
+        int phase = minors + 2 * rooks + 4 * queens;
+        if (phase > Psqt.PhaseMax) phase = Psqt.PhaseMax; // early promotions
+
+        int whitePov = (Psqt.Mg(packed) * phase + Psqt.Eg(packed) * (Psqt.PhaseMax - phase)) / Psqt.PhaseMax;
         return pos.Turn == Color.White ? whitePov : -whitePov;
     }
 }
